c44fd52eac10bb1f8ae028fbd83bcb66962c9cd3
Justen Robertson <justen@justen.us>
2017-07-20 08:35:51 -0700

    Extracted ops from app folder

--- app.js
+++ app.js
@@ -67,10 +67,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);
 app.use("/users", users);
 
-app.get("/ops", function(req, res) {
-	res.render("ops");
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
